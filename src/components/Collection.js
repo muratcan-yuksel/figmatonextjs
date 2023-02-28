61c19b57e537d61ab6fcbfb8f92f52bde4318c9a
muratcan-yuksel <muratcanyukselpro@gmail.com>
2023-03-01 01:01:16 +0300

    .

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,12 +1,15 @@
+import Image from "next/image";
 import React from "react";
 
-const Collection = () => {
+const Collection = ({ icon, title, para }) => {
   return (
-    <div>
-      <div className="bg-[#3056D3] w-20 h-20 opacity-5 ">1</div>
-      <div>
-        2<h2>title</h2>
-        <p>p</p>
+    <div className="flex flex-row my-12">
+      <div className="bg-[#3056d30d] w-20 h-20  mr-7 flex justify-center items-center">
+        <Image width="24" height="24" src={icon} alt="" />
+      </div>
+      <div className="w-full">
+        <h2 className="font-semibold text-[20px] leading-5 mb-2">{title}</h2>
+        <p className="text-[#637381] text-[16px] leading-7 w-56">{para}</p>
       </div>
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,19 @@
 import React from "react";
 import Collection from "./Collection";
+import house from "@/assets/house.png";
+import email from "@/assets/email.png";
+import phone from "@/assets/phone.png";
 
 const Contact = () => {
+  const data = [
+    {
+      title: "Our Location",
+      para: "99 S.t Jomblo Park Pekanbaru 28292. Indonesia",
+      icon: house,
+    },
+    { title: "Phone Number", para: "(+62)81 414 257 9980", icon: phone },
+    { title: "Email Address", para: "info@yourdomain.com", icon: email },
+  ];
   return (
     <div className="w-1/2 h-screen border px-28 py-32">
       <h5 className="text-[#3056D3] font-semibold text-[16px] leading-5 mb-2">
@@ -16,7 +28,14 @@ const Contact = () => {
         praesentium? Exercitationem natus optio debitis quos recusandae
         assumenda? Aperiam, accusantium id.
       </p>
-      <Collection />
+      {data.map((item, index) => (
+        <Collection
+          key={index}
+          title={item.title}
+          para={item.para}
+          icon={item.icon}
+        />
+      ))}
     </div>
   );
 };
